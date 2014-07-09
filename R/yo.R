6bf3e7403a3e5b35b940ff4920292fec4563d968
dichika <ichikawa@hcc-jp.com>
2014-07-09 23:45:07 +0900

    add yo for user

--- R/yo.R
+++ R/yo.R
@@ -1,6 +1,16 @@
-yo <- function(apitoken){
+yo <- function(apitoken=NULL, username=NULL){
   require(httr)
+  if(is.null(apitoken)){
+    stop("apitoken is needed.")
+  }
+  if(is.null(username)){
   POST("http://api.justyo.co/yoall/", 
-       body = list(api_token=apitoken)
+       body = list(api_token = apitoken)
        )
+  }else{
+    POST("http://api.justyo.co/yo/", 
+         body = list(username = username,
+                     api_token = apitoken)
+         )
+  }
 }
