9aaa4650731917923817335b4a74e104131a5ffa
ichikawadaisuke <ichikawadaisuke@ichikawadaisuke-no-iMac.local>
2014-07-10 11:12:52 +0900

    prepare for registration

--- R/yo.R
+++ R/yo.R
@@ -1,16 +1,38 @@
+#'Just Yo
+#'
+#'\code{yo} yo all subscriber or a specific subscriber. 
+#'
+#'Before yo(), you need your API key. Please visit http://yoapi.justyo.co/.
+#'
+#'@param apitoken Your Yo API token
+#'@param username Username who you want
+#'  
+#'  
+#'@return status code and status message
+#'  
+#' @examples
+#' \dontrun{
+#' # Yo all subscriber.
+#' beep("Your API token")
+#' # Yo a specific subscriber
+#' beep("Your API token", "username")
+#' }
+#'@export
 yo <- function(apitoken=NULL, username=NULL){
   require(httr)
   if(is.null(apitoken)){
     stop("apitoken is needed.")
   }
   if(is.null(username)){
-  POST("http://api.justyo.co/yoall/", 
+  res <- POST("http://api.justyo.co/yoall/", 
        body = list(api_token = apitoken)
        )
   }else{
-    POST("http://api.justyo.co/yo/", 
+    res <- POST("http://api.justyo.co/yo/", 
          body = list(username = username,
                      api_token = apitoken)
          )
   }
+  res_message <- paste(collapse=" ",res$headers$status, res$headers$statusmessage)
+  return(res_message)
 }
